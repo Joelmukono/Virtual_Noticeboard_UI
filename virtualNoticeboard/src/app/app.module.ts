cc5e19f4e1ef717a8e5abcaf21b033bf0de725e4
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-22 21:01:32 +0300

    Create login component

--- virtualNoticeboard/src/app/app.module.ts
+++ virtualNoticeboard/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoticeComponent } from './notice/notice.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NoticeComponent
+    NoticeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
