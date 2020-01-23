998e55306450fc7b6600a7b4ce11de452ad44040
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-23 14:56:10 +0300

    Add user attribute to feedback class

--- virtualNoticeboard/src/app/Feedback.ts
+++ virtualNoticeboard/src/app/Feedback.ts
@@ -1,3 +1,3 @@
 export class Feedback {
-    constructor(public feedbackId: number, public comment: string){}
+    constructor(public feedbackId: number, public comment: string, public user: string){}
 }
